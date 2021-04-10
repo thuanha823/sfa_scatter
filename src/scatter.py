15441a0c8c930b8568c84de91bc2f762d2128404
Thuan Ha <thuanha823@gmail.com>
2021-04-10 17:35:02 -0500

    Create UI and Main Layout UI

--- src/scatter.py
+++ src/scatter.py
@@ -24,4 +24,35 @@ class ScatterUI(QtWidgets.QDialog):
         self.setFixedHeight(475)
         self.setWindowFlags(self.windowFlags() ^
                             QtCore.Qt.WindowContextHelpButtonHint)
-        
+
+    def create_ui(self):
+        self.title_lbl = QtWidgets.QLabel("Scatter Tool")
+        self.title_lbl.setAlignment(QtCore.Qt.AlignCenter)
+        self.title_lbl.setStyleSheet("font: Bold 40px")
+        self.scatter_rot_lbl = QtWidgets.QLabel("Scatter Rotation Offset")
+        self.scatter_rot_lbl.setStyleSheet("font: Bold 20px")
+        self.scatter_scl_lbl = QtWidgets.QLabel("Scatter Scale")
+        self.scatter_scl_lbl.setStyleSheet("font: Bold 20px")
+        self.sct_cnl_lay = self.cancel_lay_ui()
+        self.rnd_rotation_lay = self.randomize_rotate_ui()
+        self.rnd_scale_lay = self.randomize_scale_ui()
+        self.main_layout_ui()
+
+    def main_layout_ui(self):
+        self.main_lay = QtWidgets.QVBoxLayout()
+        self.main_lay.addWidget(self.title_lbl)
+        self.main_lay.addWidget(self.scatter_rot_lbl)
+        self.main_lay.addLayout(self.rnd_rotation_lay)
+        self.main_lay.addWidget(self.scatter_scl_lbl)
+        self.main_lay.addLayout(self.rnd_scale_lay)
+        self.main_lay.addStretch()
+        self.main_lay.addLayout(self.sct_cnl_lay)
+        self.setLayout(self.main_lay)
+
+    def create_connections(self):
+        self.scatter_btn.clicked.connect(self.scatter_object)
+        self.cancel_btn.clicked.connect(self.cancel)
+        self.rot_btn.clicked.connect(self.scatter_rotate_object)
+        self.scatter_rot_connections()
+        self.scl_btn.clicked.connect(self.scatter_scale_object)
+        self.scatter_scl_connections()
